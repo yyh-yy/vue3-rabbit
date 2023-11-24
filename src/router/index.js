fdccd8297158b5af79155e19576f5090434f1853
yyh-yy <435607052@qq.com>
2023-11-24 17:37:23 +0800

    个人中心模块

--- src/router/index.js
+++ src/router/index.js
@@ -37,12 +37,27 @@ const routes = [
             {
                 path: 'paycallback',
                 component: () => import('@/views/Pay/PayBack.vue')
+            },
+            {
+                path: 'member',
+                component: () => import('@/views/Member/index.vue'),
+                children: [
+                    {
+                        path: '',
+                        component: () => import('@/views/Member/components/UserInfo.vue')
+                    },
+                    {
+                        path: 'order',
+                        component: () => import('@/views/Member/components/UserOrder.vue')
+                    }
+                ]
             }
         ]
     },
     {
         path: '/login',
-        component: () => import('@/views/Login/index.vue')
+        component: () => import('@/views/Login/index.vue'),
+
     }
 ]
 const router = createRouter({
